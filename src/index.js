ea067522678ade8b786645f009a70d4dc6dc0612
Volodymyr <mv.design8@gmail.com>
2023-04-14 03:08:39 +0300

    trands

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './sass/main.scss';
 import './js/api';
 import './js/test';
+import './js/card_markup';
+import './js/render_trends';
+import './js/genre_info';
+import './js/genres_data';
 // ...
