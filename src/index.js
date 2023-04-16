05215274326915f8b4497cd38a1ddb7e8b07b002
Kostiantyn1303 <kosty13humera@gmail.com>
2023-04-16 17:53:11 +0300

    all modal card activities

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import './js/render_trends';
 import './js/genre_info';
 import './js/genres_data';
 import './js/modal_contributor';
+import './js/modal-card_open';
+import './js/modal-card_render';
 // ...
 import './js/firebase/firebase-auth';
 import './js/firebase/auth-modal';
---
+++ src/js/modal-card_render.js
@@ -0,0 +1,71 @@
+import { FilmAPI } from './api';
+
+const modalCardRender = document.querySelector('.modal-card__render');
+
+const modalCardAPI = new FilmAPI();
+
+export default async function fetchModalCard(objectMovie) {
+  console.log(objectMovie);
+
+  const posterPath = 'https://image.tmdb.org/t/p/w500';
+  const {
+    backdrop_path,
+    poster_path,
+    original_title,
+    overview,
+    vote_average,
+    vote_count,
+    title,
+    genres,
+    popularity,
+  } = objectMovie;
+  const markup = `<div class="modal-card__img-wrapper"><img
+  class="modal-card__img"
+  src="${posterPath}${poster_path}"
+  alt="film-poster"
+  width="240"
+/> </div>
+<p class="modal-card__title">${title}</p>
+<ul class="modal-card__film-information-list">
+<div class="modal-card__film-information-items-wrrapper">
+  <li class="modal-card__film-information__items ">Vote / Votes</li>
+  <p class="modal-card__film-information__text">${vote_average}/${vote_count}</p>
+</div>
+<div class="modal-card__film-information-items-wrrapper">
+<li class="modal-card__film-information__items">
+  Popularity
+</li>
+<p class="modal-card__film-information__text">${popularity}</p></div>
+<div class="modal-card__film-information-items-wrrapper">
+  <li class="modal-card__film-information__items" >Original Title
+</li>
+<p class="modal-card__film-information__text">${original_title}</p></div>
+<div class="modal-card__film-information-items-wrrapper">
+<li class="modal-card__film-information__items">
+  Genre
+</li>
+<p class="modal-card__film-information__text">${genres.map(
+    ganre => ganre.name
+  )}</p></div>
+</ul>
+<p class="modal-card__subtitle">About</p>
+<p class="modal-card__text">${overview}
+</p><div class="modal-card__btn-wrapper">
+<button
+class="modal-card__watched-btn"
+type="button"
+data-card-modal-watched-film
+>
+add to Watched
+</button>
+<button
+class="modal-card__queue-btn"
+type="button"
+data-card-modal-queue-film
+>
+add to queue
+</button>
+</div>`;
+
+  return (modalCardRender.innerHTML = markup);
+}
