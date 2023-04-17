fe98ec018eeb904d3c139c833b300f818dd1086c
Yuliya Maslyanenko <yuliyamas86@gmail.com>
2023-04-18 00:37:16 +0300

    buttons header, margins

--- src/library.js
+++ src/library.js
@@ -1,5 +1,10 @@
 import './sass/main.scss';
 import './js/api';
+
+import './js/library_button';
+import './js/authlib_btn';
+
+
 // import './js/test';
 import './js/card_markup';
 // import './js/render_trends';
@@ -13,5 +18,3 @@ import './js/firebase/auth-modal';
 import './js/search_film';
 import './js/slider-to-top';
 
-// import './js/library_button';
-import './js/authlib_btn';
