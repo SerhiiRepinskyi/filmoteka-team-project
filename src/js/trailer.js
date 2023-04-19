b8106f4f12cb657436ad59439b5492272ef839ee
Volodymyr <115617874+VolodyMer23@users.noreply.github.com>
2023-04-19 17:43:02 +0300

    Update trailer.js

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -2,15 +2,15 @@ import { openModalTrailer } from './modal-card_markup';
 import { FilmAPI } from './api';
 
 const youtubeApi = new FilmAPI();
-const onClickTrailerBtn = document.querySelector('.modal-card__watch-video');
-console.log(youtubeApi);
 
-onClickTrailerBtn.addEventListener('click', e => {
+export function onPlayTreilerClick(e) {
   youtubeApi.youTubeID = localStorage.getItem('LOCALSTORAGE_KEY');
-  youtubeApi.fetchYouTube().then(res => {
-    const filterArray = res.results.filter(data =>
-      data.name.includes('Official')
-    );
-    openModalTrailer(filterArray[0].key);
-  });
-});
+     youtubeApi.fetchYouTube().then(res => {
+       console.log(res.results);
+       const filterArray = res.results.filter(data =>
+         data.name.includes('Official')
+       );
+       console.log(filterArray);
+       openModalTrailer(filterArray[0].key);
+     });
+}
