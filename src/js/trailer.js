1d930ba366c9bed497d99867e20945a7ba3622be
Volodymyr <115617874+VolodyMer23@users.noreply.github.com>
2023-04-19 18:38:38 +0300

    Update trailer.js

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -6,11 +6,11 @@ const youtubeApi = new FilmAPI();
 export function onPlayTreilerClick(e) {
   youtubeApi.youTubeID = localStorage.getItem('LOCALSTORAGE_KEY');
      youtubeApi.fetchYouTube().then(res => {
-       console.log(res.results);
+       
        const filterArray = res.results.filter(data =>
          data.name.includes('Official')
        );
-       console.log(filterArray);
+       
        openModalTrailer(filterArray[0].key);
      });
 }
